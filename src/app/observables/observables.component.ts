b21e595e9741ddac3a73e459a6a8ec2ddd73b6ee
Your Name <you@example.com>
2018-11-26 22:00:42 -0300

    Segunda parte del observable

--- src/app/observables/observables.component.ts
+++ src/app/observables/observables.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of, Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-observables',
@@ -11,10 +12,15 @@ export class ObservablesComponent implements OnInit {
   constructor() { 
     var observable$ = new Observable( suscriber =>{
       let array = [1,2,3,4,5];
-  
+
       array.forEach((element) => {
           suscriber.next(element);
-          suscriber.error('Aca hay un error');
+          /*
+          let source$ = of(1,2,3,4,5);
+          let doubler = map((value:number) => value * 2);
+          let doubled$ = doubler(source$);//2,4,6,8,10
+          */
+          //suscriber.error('Aca hay un error');
           //Directamente tira un error
           //throw new Error('Error');
       })
@@ -40,6 +46,33 @@ export class ObservablesComponent implements OnInit {
     subscription.unsubscribe();
 
     */
+
+    let source$ = of(1,2,3,4,5);
+    let doubler$ = source$.pipe(
+      map((value:number)=>value*2)
+    );
+
+    
+
+    //observable$.subscribe(resp => console.log(resp));
+    doubler$.subscribe(
+      resp => console.log(resp),
+      error => console.log(error),
+      () => console.log('Completed')
+    );
+
+
+    let subject$ = new Subject();
+    subject$.next('hola');
+
+    let observable2$ = of(1,2,3,4,5);
+
+    observable2$.subscribe(subject$);
+
+    subject$.subscribe(value => console.log(value));    
+
+
+
   }
 
   ngOnInit() {
